4377bd25b0f10331e8acf0df1293a0222effdade
yangshangwei <yangshangwei@inspur.com>
2019-03-22 00:02:58 +0800

    初始化提交

---
+++ artisan-product/src/main/java/com/artisan/product/ArtisanProductApplication.java
@@ -0,0 +1,15 @@
+package com.artisan.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ArtisanProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtisanProductApplication.class, args);
+    }
+
+}
