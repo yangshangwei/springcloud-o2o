47162541278657a124ee75124d0e9279e16de019
yangshangwei <yangshangwei@inspur.com>
2019-04-04 10:29:02 +0800

    config server： 注册到注册中心

---
+++ artisan_config/src/main/java/com/artisan/config/ArtisanConfigApplication.java
@@ -0,0 +1,16 @@
+package com.artisan.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ArtisanConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtisanConfigApplication.class, args);
+    }
+
+}
