5a5915fb91c2ce9db51f4191c9b395dc37ee67c2
yangshangwei <yangshangwei@inspur.com>
2019-04-10 09:40:31 +0800

    使用/actuator/bus-refresh端点手动刷新配置 + 使用Spring Cloud Bus自动更新配置

--- artisan_config/src/main/java/com/artisan/config/ArtisanConfigApplication.java
+++ artisan_config/src/main/java/com/artisan/config/ArtisanConfigApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ArtisanConfigApplication {
 
     public static void main(String[] args) {
