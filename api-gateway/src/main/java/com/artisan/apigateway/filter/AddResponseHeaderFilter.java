5174fa11096c24b578942523339948b589d26ae0
yangshangwei <yangshangwei@inspur.com>
2019-04-15 09:12:14 +0800

    api-gateway

---
+++ api-gateway/src/main/java/com/artisan/apigateway/filter/AddResponseHeaderFilter.java
@@ -0,0 +1,39 @@
+package com.artisan.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.UUID;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+
+
+@Component
+public class AddResponseHeaderFilter extends ZuulFilter{
+    @Override
+    public String filterType() {
+        return POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletResponse response = requestContext.getResponse();
+        response.setHeader("X-Foo", UUID.randomUUID().toString());
+        return null;
+    }
+}
